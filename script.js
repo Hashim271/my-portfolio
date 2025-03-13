a000e73117225516995b84586161939a0705202c
Hashim271 <hashim46@live.ca>
2025-03-13 13:03:18 -0400

    Added placeholders on form and Made the Script so it does not submit untill everything is valid

--- script.js
+++ script.js
@@ -16,3 +16,60 @@ buttons.forEach(button => {
         }
     });
 });
+
+//Make sure the name input only accepts letters and no numbers
+const nameInput = document.getElementById('name');
+
+nameInput.addEventListener('input', function (e) {
+    const inputValue = e.target.value;
+
+    if (/\d/.test(inputValue)) {
+        alert('Numbers are not allowed in the name field! Please enter a valid name.');
+
+        e.target.value = inputValue.replace(/\d/g, '');
+
+        e.preventDefault();
+        return;
+    }
+});
+
+//Make sure the email input is valid
+const emailInput = document.getElementById('email');
+const form = document.getElementById('form');
+
+function validateEmail(email) {
+    const emailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailValid.test(email);
+}
+
+//Make sure that the subject input and the comment section is of minimum 5 char to avoid spam.
+function validateMinLength(value, minLength) {
+    return value.trim().length >= minLength;
+}
+
+//Make sure all input are good before submitting
+form.addEventListener('submit', function (e){
+    const emailValue = emailInput.value;
+    const subjectInput = document.getElementById('subject');
+    const commentInput = document.getElementById('comment');
+    let isValid = true;
+
+    if(!validateEmail(emailValue)) {
+        alert('Please enter a valid email address!');
+        isValid = false;
+    }
+
+    if (!validateMinLength(subjectInput.value, 3)) {
+        alert('Subject needs to be at least 3 characters long to avoid spam!');
+        isValid = false;
+    }
+
+    if (!validateMinLength(commentInput.value, 5)) {
+        alert('Comment needs to be at least 5 characters long to avoid spam!')
+        isValid = false;
+    }
+
+    if (!isValid) {
+        e.preventDefault();
+    }
+});
